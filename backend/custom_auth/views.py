f32d2933553c31f610654254208519e55889cced
Botalov Sergey <89090168690@mail.ru>
2025-08-05 22:06:05 +0500

    added logout

--- backend/custom_auth/views.py
+++ backend/custom_auth/views.py
@@ -1,9 +1,13 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+
+from django.http import HttpRequest
+from django.conf import settings
+from django.contrib.auth import logout
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import jwt
-from django.conf import settings
-from datetime import datetime, timedelta, timezone
 
 from .models import CustomUser
 from .serializers import UserSerializer
@@ -15,14 +19,21 @@ class RegisterView(APIView):
         if serializer.is_valid():
             user = serializer.save()
             return Response(
-                {"message": "User registered"},
+                {
+                    "message": "User registered",
+                    "user_id": user.pk,
+                    "email": user.email,
+                },
                 status=status.HTTP_201_CREATED,
             )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class LoginView(APIView):
-    def post(self, request):
+    def post(self, request: HttpRequest):
         email = request.data.get("email")
         password = request.data.get("password")
         try:
@@ -31,13 +42,17 @@ class LoginView(APIView):
 
                 token = jwt.encode(
                     {
-                        "user_id": user.id,
+                        "user_id": user.pk,
+                        "email": user.email,
                         "exp": datetime.now(timezone.utc) + timedelta(hours=24),
                     },
                     settings.JWT_SECRET,
                     algorithm="HS256",
                 )
-                return Response({"token": token}, status=status.HTTP_200_OK)
+                return Response(
+                    {"token": token},
+                    status=status.HTTP_200_OK,
+                )
             return Response(
                 {"error": "Invalid credentials"},
                 status=status.HTTP_401_UNAUTHORIZED,
@@ -47,3 +62,17 @@ class LoginView(APIView):
                 {"error": "Invalid credentials"},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
+
+
+class LogoutView(APIView):
+    def post(self, request: HttpRequest):
+        if request.user and not isinstance(request.user, AnonymousUser):
+            logout(request)
+            return Response(
+                {"message": "Successfully logged out."},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"error": "Not authenticated"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
