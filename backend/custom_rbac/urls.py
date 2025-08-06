859b93b0c8b62eb9ff06eeed518b382d5a4ff041
Botalov Sergey <89090168690@mail.ru>
2025-08-06 22:15:58 +0500

    added update roles

--- backend/custom_rbac/urls.py
+++ backend/custom_rbac/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AccessRolesRuleView, BusinessElementView, RoleView
+from .views import AccessRolesRuleView, BusinessElementView, RoleDetailView, RoleView
 
 urlpatterns = [
     path("roles/", RoleView.as_view(), name="roles"),
+    path("roles/<int:role_id>/", RoleDetailView.as_view(), name="roles"),
     path("elements/", BusinessElementView.as_view(), name="business-element"),
     path("access-roles/", AccessRolesRuleView.as_view(), name="access-roles"),
 ]
--- backend/custom_rbac/views.py
+++ backend/custom_rbac/views.py
@@ -38,6 +38,30 @@ class RoleView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class RoleDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def patch(self, request: HttpRequest, role_id: int):
+        if not request.user.is_authenticated or request.user.role.name != "admin":
+            return Response(
+                {"error": "Forbidden"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        try:
+            role = Role.objects.get(id=role_id)
+        except Role.DoesNotExist:
+            return Response(
+                {"error": "Role not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = RoleSerializer(role, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class BusinessElementView(APIView):
     authentication_classes = [JWTAuthentication]
 
